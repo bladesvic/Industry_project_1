62c66eb93f660dd5dbb573461267ecc15d3b4e81
bladesvic <35073656+bladesvic@users.noreply.github.com>
2024-11-18 14:18:03 +0800

    Update authController.js

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -12,7 +12,7 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "10000h" });
     res.json({ token });
   } catch (error) {
     res.status(500).json({ message: "An error occurred during login" });
